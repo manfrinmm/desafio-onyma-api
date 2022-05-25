aafa2759c31491f7b2e34ebaad540083881b56e7
Manfrin <matheus_mm123@hotmail.com>
2022-05-24 21:46:45 -0300

    basic project

---
+++ src/domain/@shared/events/impls/EventDispatcher.spec.ts
@@ -0,0 +1,9 @@
+import EventDispatcher from "./EventDispatcher";
+
+describe('Domain events tests', () => {
+
+  test.todo('should register and event handler');
+  test.todo('should unregister an event handler');
+  test.todo('should unregister all event handlers');
+  test.todo('should notify all event handlers');
+});
