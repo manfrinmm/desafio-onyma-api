9a0fed735c5ee73f063abe62fff69926038c0e6b
Manfrin <matheus_mm123@hotmail.com>
2022-05-27 12:56:35 -0300

    remove files unused

--- arq/repositories/RepositoryInterface.ts
+++
@@ -1,8 +0,0 @@
-// Repositórios tem ligação um-pra-um com agregados
-// T = Agregado
-export default interface IRepositoryInterface<T> {
-  create(entity: T): Promise<T>;
-  update(entity: T): Promise<T>;
-  find(id: string): Promise<T | null>;
-  findAll(): Promise<T[]>;
-}
